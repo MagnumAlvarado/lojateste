15e35437e6ea64e7217390136ff46e71c9f2b6b8
Magnum <magnum.alvarado@gmail.com>
2021-07-16 21:26:29 -0300

    main and index change

--- main.js
+++ main.js
@@ -1,4 +1,33 @@
-function salvarEmail(){
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	let nome = document.getElementById('nome').value;
+	let email = document.getElementById('email').value;
+	let data = {
+		nome,
+		email
+	}
+	
+	let convertData = JSON.stringify(data);
+
+	localStorage.setItem('lead', convertData)
+
+	let content = document.getElementById('content')
+
+	let carregando = `<p>Carregando...</p>`
+
+	let sucesso = `<h2 style="color: #4f38b9">Obrigado por cadastrar! Continue navegando, aproveite e visite o Instagran.</h2>`
+
+	content.innerHTML = carregando
+
+	setTimeout(() => {
+		content.innerHTML = sucesso
+	}, 1000)
+
+})
+
+/*function salvarEmail(){
   var textEmail = document.getElementById("email");
   var email = textEmail.value;
 
