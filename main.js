b0ad7b1b465752fbbf1e193e5e7dd365f92d9e04
Magnum <magnum.alvarado@gmail.com>
2021-07-16 22:59:21 -0300

    autalizado main

--- main.js
+++ main.js
@@ -26,36 +26,3 @@ form.addEventListener('submit', (e) => {
 	}, 1000)
 
 })
-
-/*function salvarEmail(){
-  var textEmail = document.getElementById("email");
-  var email = textEmail.value;
-
-  localStorage.setItem("email",email);
-
-  alert("Tudo pronto para você ficar por dentro das melhores ofertas")
-}
-
-
-/* ScrollReveal: Mostrar elementos quando der scroll na página */
-/*const scrollReveal = ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 700,
-    reset: true
-  })
-  
-  scrollReveal.reveal(
-    `#home .image, #home .text,
-    #about .image, #about .text,
-    #services header, #services .card,
-    #testimonials header, #testimonials .testimonials
-    #contact .text, #contact .links,
-    footer .brand, footer .social
-    `,
-    { interval: 100 }
-  )
-  
-  var btnRegistar = document.getElementById("btnRegistrar")
-btnRegistar.onclick = salvarEmail;
-
